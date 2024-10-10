2855d06e7d1f9bb8b3a2f7309f3a5d72e9af0953
Samuel W. Flint <swflint@flintfam.org>
2024-10-10 13:43:18 -0500

    Add a commentary and readme

--- org-inline-pdfcomment.el
+++ org-inline-pdfcomment.el
@@ -1,7 +1,7 @@
 ;;; org-inline-pdfcomment.el --- Export Support for Inline Tasks as PDF Comments  -*- lexical-binding: t; -*-
 
 ;; Author: Samuel W. Flint <me@samuelwflint.com>
-;; Version: 0.0.1
+;; Version: 1.0.0
 ;; Homepage: https://git.sr.ht/~swflint/org-inline-pdfcomment
 ;; Keywords: docs, text
 ;; Package-Requires: ((emacs "24.4"))
@@ -23,11 +23,35 @@
 
 ;;; Commentary:
 
-;; TODO
+;; This package provides support for exporting org-inlinetask elements
+;; as PDF comments using the pdfcomment.sty package for LaTeX
+;; (https://ctan.org/pkg/pdfcomment).  It may be enabled as follows:
+;;
+;;     (require 'org-inline-pdfcomment)
+;;     (org-inline-pdfcomment-insinuate)
+;;
+;; Inline tasks will then be exported as PDF comments.
+;;
+;; **NOTE**: Loading this packages will load `org-inlinetask' if it is
+;; not loaded.
+;;
+;;;; Output Customization
+;;
+;;  - `org-inline-pdfcomment-type' can be either of the symbols
+;;    `inline' or `margin', or a string which will be used as the
+;;    comment command.
+;;  - `org-inline-pdfcomment-options' is an alist of pdfcomment
+;;    options.  For more information about these, see the docstring or
+;;    the pdfcomment.sty manual.
+;;  - `org-inline-pdfcomment-format-todo-function' is a function used
+;;    to format the todo state.
+;;  - `org-inline-pdfcomment-format-tags-function' is a function used
+;;    to format the task tags.
 
 ;;; Code:
 
 (require 'ox-latex)
+(require 'org-inlinetask)
 
 
 ;;; Customizations
